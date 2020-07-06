6d957bb4b5abcb11fe9746295c9f8fc1f47de54c
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-06 22:32:05 +0200

    Add event listeners to action dispatcher

--- src/Poem/Actor/ActionDispatcher.php
+++ src/Poem/Actor/ActionDispatcher.php
@@ -2,12 +2,15 @@
 
 namespace Poem\Actor;
 
+use Poem\Actor\Exceptions\ActionException;
+use Psr\Http\Message\ResponseInterface;
 use Slim\App;
 
 class ActionDispatcher {
     private $baseRoute;
     private $actions = [];
-    private $subjectClass;
+    public $subjectClass;
+    private $listeners = [];
 
     function __construct($baseRoute = null, $subjectClass = null) {
         $this->baseRoute = $baseRoute;
@@ -18,31 +21,53 @@ class ActionDispatcher {
         $this->actions[$actionClass] = $configurator;
     }
 
+    function addListener($name, callable $hook) {
+        if(!isset($this->listeners[$name])) {
+            $this->listeners[$name] = [];
+        }
+
+        $this->listeners[$name][] = $hook;
+    }
+
     function dispatch(App $app) {
-        $subjectClass = $this->subjectClass;
+        $dispatcher = $this;
 
         foreach($this->actions as $actionClass => $configurator) {
             $method = $actionClass::getMethod();
             $route = $actionClass::getRoute();
 
             $app->{$method}('/' . $this->baseRoute . $route, 
-                function($request, $response, $args) use($actionClass, $configurator, $subjectClass) {
+                function($request, ResponseInterface $response, $args) use($actionClass, $configurator, $dispatcher) {
                     /** @var Action $action */
                     $action = new $actionClass();
 
-                    if($subjectClass) {
-                        $action->setSubject($subjectClass);
+                    if($dispatcher->subjectClass) {
+                        $action->setSubject($dispatcher->subjectClass);
                     }
 
                     if(is_callable($configurator)) {
                         $configurator($action);
                     }
 
-                    if(!$action->canActivate()) {
-                        // Return 401 Unauthorized
+                    try {
+
+                        if(isset($dispatcher->listeners['before'])) {
+                            foreach($dispatcher->listeners['before'] as $callback) {
+                                $callback($action, $request);
+                            }
+                        }
+
+                        // Before action hooks
+                        /** @var ResponseInterface $response */
+                        $response = call_user_func($action, $request, $response, $args);
+
+                        // After action hooks
+
+                    } catch(ActionException $e) {
+                        return $response->withStatus($e->getCode());
                     }
 
-                    return call_user_func($action, $request, $response, $args);
+                    return $response;
                 }
             );
         }
