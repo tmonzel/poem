c0784b42488fa53883e8968a658c9a3ed15f7dc8
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-09 10:49:42 +0200

    Make Set convertable to related data

--- src/Poem/Model/Set.php
+++ src/Poem/Model/Set.php
@@ -5,5 +5,9 @@ namespace Poem\Model;
 use Poem\Set as BaseSet;
 
 class Set extends BaseSet {
-    
+    function toRelatedData() {
+        return $this->map(function($document) {
+            return $document->toRelatedData();
+        });
+    }
 }
