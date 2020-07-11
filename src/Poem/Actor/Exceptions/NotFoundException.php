81d9f413c89aa2f46ff5870dc5f926fc10f8d3dd
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-11 06:15:24 +0200

    Add exception constructor

--- src/Poem/Actor/Exceptions/NotFoundException.php
+++ src/Poem/Actor/Exceptions/NotFoundException.php
@@ -3,5 +3,7 @@
 namespace Poem\Actor\Exceptions;
 
 class NotFoundException extends ActionException {
-
+    function __construct($message, $errors = []) {
+        parent::__construct($message, 404, $errors);
+    }
 }
