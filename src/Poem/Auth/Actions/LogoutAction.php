2e162186022afee2507d2e242b4797c2bcd03019
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-14 16:37:23 +0200

    Cleanup logout action

--- src/Poem/Auth/Actions/LogoutAction.php
+++ src/Poem/Auth/Actions/LogoutAction.php
@@ -3,12 +3,12 @@
 namespace Poem\Auth\Actions;
 
 use Poem\Actor\Action;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 class LogoutAction extends Action {
     static $type = 'logout';
 
-    function prepareData(Request $request) {
+    function prepareData() 
+    {
         
     }
 }
