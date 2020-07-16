b1e69d1d3e7dfcd40d91a8df2e88342dd64dec7f
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-16 12:17:14 +0200

    Implement new find action behavior

--- src/Poem/Actor/Actions/FindAction.php
+++ src/Poem/Actor/Actions/FindAction.php
@@ -4,11 +4,38 @@ namespace Poem\Actor\Actions;
 
 use Poem\Actor\Action;
 
+/**
+ * This action fetches data of a given subject
+ */
 class FindAction extends Action {
+
+    /**
+     * Find action type
+     * 
+     * @static
+     * @var string
+     */
     static $type = 'find';
 
+    /**
+     * Data preparation
+     */
     function prepareData() 
     {
-        return $this->subject::find();
+        if(isset($this->payload['id'])) {
+            // Return a single document
+            $document = $this->subject::pick((int)$this->payload['id']);
+            return $document->toData($this->payload);
+        }
+        
+        $data = [];
+        $conditions = isset($this->payload['conditions']) ? $this->payload['conditions'] : [];
+        $documents = $this->subject::find($conditions);
+
+        foreach($documents as $d) {
+            $data[] = $d->toData($this->payload);
+        }
+
+        return $data;
     }
 }
