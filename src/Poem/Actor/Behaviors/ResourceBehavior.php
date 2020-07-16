a58b1e6f39fcf3423939e133245356c57bbd5d61
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-16 12:17:37 +0200

    Remove pick action

--- src/Poem/Actor/Behaviors/ResourceBehavior.php
+++ src/Poem/Actor/Behaviors/ResourceBehavior.php
@@ -6,7 +6,6 @@ use Poem\Actor\ActionQuery;
 use Poem\Actor\Actions\CreateAction;
 use Poem\Actor\Actions\DestroyAction;
 use Poem\Actor\Actions\FindAction;
-use Poem\Actor\Actions\PickAction;
 use Poem\Actor\Actions\UpdateAction;
 use Poem\Actor\Behavior;
 
@@ -15,7 +14,6 @@ class ResourceBehavior extends Behavior {
         $this->registerAction(FindAction::class);
         $this->registerAction(DestroyAction::class);
         $this->registerAction(UpdateAction::class);
-        $this->registerAction(PickAction::class);
         $this->registerAction(CreateAction::class);
     }
 }
