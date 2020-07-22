54c971efc58b896ca77f890254dc166a1d5f2ff5
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-22 17:04:05 +0200

    Fix update action

--- src/Poem/Actor/Actions/UpdateAction.php
+++ src/Poem/Actor/Actions/UpdateAction.php
@@ -5,6 +5,7 @@ namespace Poem\Actor\Actions;
 use Poem\Actor\Action;
 use Poem\Actor\AttributeMapper;
 use Poem\Actor\Exceptions\BadRequestException;
+use Poem\Actor\Exceptions\NotFoundException;
 use Poem\Model;
 
 class UpdateAction extends Action {
@@ -18,18 +19,23 @@ class UpdateAction extends Action {
             throw new BadRequestException('id must be provided in payload');
         }
 
-        if(!isset($this->payload['data'])) {
-            throw new BadRequestException('data must be provided in payload');
+        if(!isset($this->payload['attributes'])) {
+            throw new BadRequestException('Attributes must be provided in payload');
         }
 
         $id = $this->payload['id'];
-        $attributes = $this->payload['data'];
+        $attributes = $this->payload['attributes'];
         
         /** @var Model $document */
         $document = $this->subject::pick($id);
+        
+        if(!$document) {
+            throw new NotFoundException("Document with #$id not found");
+        }
+        
         $document->writeAttributes($this->map($attributes));
         $document->save();
         
-        return $document;
+        return $document->toData($this->payload);
     }
 }
