1f78f36219e9c7192346330194fc962a17c0bb11
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-07-28 17:06:29 +0200

    Refactor update action

--- src/Poem/Actor/Actions/UpdateAction.php
+++ src/Poem/Actor/Actions/UpdateAction.php
@@ -8,32 +8,50 @@ use Poem\Actor\Exceptions\BadRequestException;
 use Poem\Actor\Exceptions\NotFoundException;
 use Poem\Model;
 
-class UpdateAction extends Action {
+class UpdateAction extends Action 
+{
     use AttributeMapper;
 
+    /**
+     * Update action type
+     * 
+     * @static
+     * @var string
+     */
     static $type = 'update';
 
+    /**
+     * Prepare action data
+     * 
+     * @return mixed
+     */
     function prepareData() 
     {
         if(!isset($this->payload['id'])) {
             throw new BadRequestException('id must be provided in payload');
         }
 
-        if(!isset($this->payload['attributes'])) {
-            throw new BadRequestException('Attributes must be provided in payload');
-        }
-
         $id = $this->payload['id'];
-        $attributes = $this->payload['attributes'];
         
         /** @var Model $document */
         $document = $this->subject::pick($id);
+
+        if(isset($this->payload['relationships'])) {
+            foreach($this->payload['relationships'] as $name => $data) {
+                $relationship = $document->getConnectedRelationship($name);
+            }
+        }
         
         if(!$document) {
             throw new NotFoundException("Document with #$id not found");
         }
         
-        $document->writeAttributes($this->map($attributes));
+        if(isset($this->payload['attributes'])) {
+            $document->writeAttributes(
+                $this->map($this->payload['attributes'])
+            );
+        }
+
         $document->save();
         
         return $document->toData($this->payload);
