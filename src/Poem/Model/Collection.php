b4a02fbe9f5a0359ced9029714ca316db470da85
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-08-05 11:39:00 +0200

    Add model collection class

---
+++ src/Poem/Model/Collection.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace Poem\Model;
+
+use Exception;
+use Poem\Actable;
+use Poem\Data\Accessor as DataAccessor;
+use Poem\Data\CollectionAdapter;
+use Poem\Module;
+
+class Collection
+{
+    use Module,
+        DataAccessor,
+        Relationships,
+        Actable;
+
+    const BEFORE_SAVE_EVENT = 'model_before_save';
+
+    /**
+     * Applied adapter
+     * 
+     * @var CollectionAdapter
+     */
+    protected $adapter;
+
+    /**
+     * Collection type
+     * 
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Client connection name
+     * 
+     * @static
+     * @var string
+     */
+    static $clientKey = 'default';
+
+    /**
+     * Primary key column
+     * 
+     * @static
+     * @var string
+     */
+    static $primaryKey = 'id';
+
+    /**
+     * Used document class
+     * 
+     * @var string
+     */
+    protected $documentClass;
+
+    /**
+     * Create a new model instance
+     * 
+     * @param string $type
+     * @param CollectionAdapter $adapter
+     */
+    function __construct(string $type, CollectionAdapter $adapter)
+    {
+        $this->type = $type;
+        $this->adapter = $adapter;
+
+        // Initialize behaviors once per class
+        static::initializeBehaviors();
+        
+        // Initialize relationships once per class 
+        static::initializeRelationships();
+    }
+
+    /**
+     * Return the foreign key for this collection
+     * 
+     * @static
+     * @return string
+     */
+    static function foreignKey(): string 
+    {
+        $className = get_called_class();
+        return strtolower(substr($className, 0, strrpos($className, '\\'))) . "_" . static::$primaryKey;
+    }
+
+    /**
+     * Guessing the document class for this collection
+     * 
+     * @return string
+     */
+    function guessDocumentClass(): string
+    {
+        if(isset($this->documentClass)) {
+            return $this->documentClass;
+        }
+
+        $this->documentClass = Document::class;
+
+        static::withNamespaceClass('Document', function($documentClass) {
+            $this->documentClass = $documentClass;
+        });
+
+        return $this->documentClass;
+    }
+
+    /**
+     * Builds a new document with the given attributes
+     * 
+     * @param array $attributes
+     * @return Document
+     */
+    function buildDocument(array $attributes = []): Document
+    {
+        $documentClass = $this->guessDocumentClass();
+        return new $documentClass($this->type, $attributes);
+    }
+
+    /**
+     * Update document if exists, 
+     * otherwise create a new one
+     * 
+     * @param Document $document
+     * @return mixed
+     */
+    function save(Document $document) 
+    {
+        $this->dispatchEvent(self::BEFORE_SAVE_EVENT, [$document]);
+        
+        if($document->exists()) {
+            // Update existing document
+            return $this->update(
+                $document->toArray(), 
+                [static::$primaryKey => $document->id]
+            );
+
+        } else {
+            // Create new document
+            $insertId = $this->adapter->insert(
+                $document->toArray()
+            );
+
+            $document->id = $insertId;
+            return true;
+        }
+    }
+
+    /**
+     * Prepare schema for migration
+     * 
+     * @return array
+     */
+    static function prepareSchema(): array 
+    {
+        $calledClass = get_called_class();
+        $schema = [];
+
+        if(defined($calledClass . '::Schema')) {
+            foreach($calledClass::Schema as $name => $type) {
+                if(class_exists($type)) {
+                    $schema[$type::foreignKey()] = 'fk';
+                } else {
+                    $schema[$name] = $type;
+                }
+            }
+        }
+
+        return $schema;
+    }
+
+    /**
+     * Picks a single document
+     * 
+     * @return Document
+     */
+    function pick(int $id) 
+    {
+        return $this->first(compact('id'));
+    }
+
+    /**
+     * Creates a find query
+     * 
+     * @return FindQuery
+     */
+    function find(): FindQuery 
+    {        
+        $query = new FindQuery(function(FindQuery $query) {
+            $conditions = $query->getConditions();
+            $result = $this->adapter->find($conditions['filter']);
+            $format = isset($conditions['format']) ? $conditions['format'] : null;
+
+            $result->map(function($attributes) use($format) {
+                $document = $this->buildDocument($attributes);
+                $document->setFormat($format);
+                
+                return $document;
+            });
+
+            return $result;
+        });
+
+        return $query;
+    }
+
+    /**
+     * Find the first occurrent document
+     * 
+     * @param array $filter
+     * @return Document|null
+     */
+    function first(array $filter = []): ?Document
+    {
+        $query = $this->find();
+        $query->filter($filter);
+        $query->limit(1);
+
+        return $query->first();
+    }   
+
+    /**
+     * Create a new document instance
+     * 
+     * @param array $attributes
+     * @return Document
+     */
+    function create(array $attributes): Document 
+    {
+        $insertId = $this->adapter->insert(
+            $attributes
+        );
+
+        return $this->buildDocument($attributes + [static::$primaryKey => $insertId]);
+    }
+
+    /**
+     * Update many documents
+     * 
+     * @param array $filter
+     * @param array $attributes
+     * @return int affected rows or throw error (TODO)
+     */
+    function update(array $filter, array $attributes) 
+    {
+        return $this->adapter->update($filter, $attributes);
+    }
+
+    /**
+     * Delete many documents
+     * 
+     * @param array $filter
+     * @return int affected rows or throw error (TODO)
+     */
+    function delete(array $filter) 
+    {
+        return $this->adapter->delete($filter);
+    }
+
+    /**
+     * Syncronize the schema for this model
+     */
+    function sync() 
+    {
+        $calledClass = get_called_class();
+
+        if(!defined($calledClass . '::Schema')) {
+            throw new Exception('No schema defined for ' . static::class);
+        }
+
+        /*$collection = static::collection();
+        
+        if($collection->exists()) {
+            // sync
+            $collection->sync(static::prepareSchema());
+        } else {
+            static::connection()->createCollection($calledClass::Type, static::prepareSchema());
+        }*/
+    }
+}
