d92bf27fdf6c9b9763d77209cee25935f73b160b
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-08-05 12:43:23 +0200

    Refactor interface

--- src/Poem/Data/CollectionAdapter.php
+++ src/Poem/Data/CollectionAdapter.php
@@ -4,18 +4,8 @@ namespace Poem\Data;
 
 interface CollectionAdapter
 {
-    function insert(array $document);
-    function insertMany(array $documents);
-
-    function findFirst(array $conditions = []);
-    function findMany(array $conditions = []);
-
-    function updateFirst(array $data, array $conditions = []);
-    function updateMany(array $data, array $conditions = []);
-
-    function deleteFirst(array $conditions = []);
-    function deleteMany(array $conditions = []);
-
-    function exists(): bool;
-    function sync(array $schema);
+    function find(array $filter = [], array $options = []): Cursor;
+    function insert(array $data, array $options = []);
+    function delete(array $filter, array $options = []);
+    function update(array $filter, array $data, array $options = []);
 }
