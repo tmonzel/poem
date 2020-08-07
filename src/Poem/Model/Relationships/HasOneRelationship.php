b7672bd86742dee1a404f0aac928bfc285a93e7e
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-08-07 19:55:04 +0200

    Implement HasOneRelationship

--- src/Poem/Model/Relationships/HasOneRelationship.php
+++ src/Poem/Model/Relationships/HasOneRelationship.php
@@ -2,9 +2,42 @@
 
 namespace Poem\Model\Relationships;
 
+use Poem\Data\Statement;
+use Poem\Model\Collection;
+use stdClass;
+
 /**
  * Proxy for has one relations
  */
-class HasOneRelationship extends Relationship {
-    
+class HasOneRelationship extends Relationship 
+{
+    function attachTo(Collection $collection, Statement $statement) 
+    {
+        $query = $this->find();
+        $foreignKey = $collection->foreignKey();
+        $target = $this->getTargetCollection()->getName();
+        $resultMap = [];
+
+        foreach($query as $document) {
+            $foreignId = $document->{$foreignKey};
+            
+            if(isset($resultMap[$foreignId])) {
+                continue;
+            }
+
+            $document->setFormat(['id', 'type']);
+
+            $resultMap[$foreignId] = $document;
+        }
+
+        $statement->addMapper(function($row) use($resultMap, $target) {
+            if(isset($resultMap[$row['id']])) {
+                $row[$target] = $resultMap[$row['id']];
+            } else {
+                $row[$target] = new stdClass;
+            }
+
+            return $row;
+        });
+    }
 }
