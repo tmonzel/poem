cc55c58df54c99ea81c3bd94b95c01489166f336
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-08-08 08:53:18 +0200

    Cleanup/Adding comments

--- src/Poem/Actor/ActionQuery.php
+++ src/Poem/Actor/ActionQuery.php
@@ -7,22 +7,55 @@ use Poem\Actor;
 
 class ActionQuery implements JsonSerializable 
 {
+    /**
+     * Owning actor instance
+     * 
+     * @var Actor
+     */
     protected $actor;
+
+    /**
+     * Action type
+     * 
+     * @var string
+     */
     protected $actionType;
+
+    /**
+     * Action payload
+     * 
+     * @var array
+     */
     protected $payload = [];
 
-    function __construct(Actor $actor, string $actionType, $payload = []) 
+    /**
+     * 
+     * @param Actor $actor
+     * @param string $actionType
+     * @param array $payload
+     */
+    function __construct(Actor $actor, string $actionType, array $payload = []) 
     {
         $this->actor = $actor;
         $this->actionType = $actionType;
         $this->payload = $payload;
     }
 
+    /**
+     * Execute this action
+     * 
+     * @return mixed
+     */
     function execute() 
     {
         return $this->actor->executeAction($this->actionType, $this->payload);
     }
 
+    /**
+     * Serialize to json
+     * 
+     * @return mixed
+     */
     function jsonSerialize()
     {
         return $this->execute();
