09c12b06478329ae734f067e09545c80355b385a
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-08-19 10:03:19 +0200

    Add user password mutation

--- app/Modules/User/Model.php
+++ app/Modules/User/Model.php
@@ -2,16 +2,21 @@
 
 namespace Modules\User;
 
-class Model extends \Poem\Auth\User 
+class Model extends \Poem\Model 
 {
     /**
-     * User validations
+     * Initialize user model
      * 
-     * @return array
+     * @return void
      */
     function initialize(): void
     {
         $this->addValidation('name', 'required');
         $this->addValidation('password', 'required');
+
+        // Add mutator for password attribute
+        $this->mutateAttribute('password', function($value) {
+            return password_hash($value, PASSWORD_ARGON2I);
+        });
     }
 }
