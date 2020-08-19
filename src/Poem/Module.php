f067668bf483fa676f59a2cd4d190ddfd5fa7581
Thomas Monzel <t.monzel@gds-gmbh.de>
2020-08-19 10:00:46 +0200

    Remove unused register method

--- src/Poem/Module.php
+++ src/Poem/Module.php
@@ -2,8 +2,6 @@
 
 namespace Poem;
 
-use Poem\Module\Worker;
-
 class Module
 {
     use ModuleHelper;
@@ -18,28 +16,5 @@ class Module
     {
         $namespace = static::getNamespace();
         return strtolower(substr($namespace, strrpos($namespace, '\\') + 1));
-    }
-
-    /**
-     * Initializes this actor on configuration layer
-     * Also called on Module\Worker::register()
-     * 
-     * @param Worker $worker
-     */
-    static function register(Worker $worker): void
-    {
-        /*$modelClass = static::$modelClass ?? static::getNamespaceClass('Model');
-        $documentClass = static::getNamespaceClass('Document');
-        
-        static::Model()->register(static::getType(), $modelClass ?? Model::class, [
-            'name' => static::getName(),
-            'relationships' => static::getRelationships(),
-            'documentClass' => $documentClass
-        ]);
-
-        static::Model()->addInitializer(
-            static::getType(), 
-            get_called_class() . '::withModel'
-        );*/
     }
 }
